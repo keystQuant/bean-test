5cb214d169faab3df6815624733773e2de54ad7f
1 <ppark9553@gmail.com>
2018-09-02 12:25:11 +0900

    db.sqlite3 돌리기

--- practice/settings.py
+++ practice/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'practice',
-        'USER': 'practice',
-        'PASSWORD': 'xptmxm123',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
